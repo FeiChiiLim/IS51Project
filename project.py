a8caf36fa92699a768198ca21d9433365917f979
Raven <raven@Ravens-MacBook-Air.local>
2020-12-09 21:49:56 +0800

    finish pseudo-code

--- project.py
+++ project.py
@@ -5,7 +5,37 @@ Set amount of guesses at 0.
 Then it will allow user to input a number.
 If the input number is smaller than the generated number, then print to user "Number too low. Guess again!"
 If the input number is greater than the generated number, then print to user "Number too high. Guess again!"
-If the user did not input a number, then print to user "You did not enter a number. Try again!"
+If the user did not input a number, then print to user "You did not enter a number. Try again"
 When the input number matches the randomly generated number, then print to user "Correct! You win!"
 and print out the amount of guesses the user took the get the correct number.
+"""
+
+
+"""
+import random
+correctAnswer = random.randint(1,100)
+gameOver = False
+count = 0
+"I've thought of a number from 1 through 100."
+
+while gameOver is False:
+    try:
+        playerGuess = int(input("Guess the number: "))
+    except ValueError:
+        "You did not  enter a number. Try again."
+    
+    if playerGuess is correctAnswer:
+        compareAnswer = "Correct"
+    elif playerGuess greater than correctAnswer:
+        compareAnswer = "Too High"
+    elif playerGuess smaller than correctAnswer:
+        compareAnswer = "Too Low"
+    count +=1
+
+    if compareAnswer is "Correct":
+        "Correct! You win!", "You took", count, "guesses."
+    elif compareAnswer is "Too High":
+        "Number too high. Guess again!"
+    else compareAnswer is "Too Low":
+        "Number too low. Guess again!"
 """
