f1d9c8124e71116ea4ff6d37d3e29f2a9ab8c412
Raven <raven@Ravens-MacBook-Air.local>
2020-12-09 18:59:26 +0800

    finish structured-english

---
+++ project.py
@@ -0,0 +1,11 @@
+"""
+This program lets user to guess the correct number from a randomly generated number between 1 to 100 from the program.
+First, the program will randomly select a number from 1 through 100.
+Set amount of guesses at 0.
+Then it will allow user to input a number.
+If the input number is smaller than the generated number, then print to user "Number too low. Guess again!"
+If the input number is greater than the generated number, then print to user "Number too high. Guess again!"
+If the user did not input a number, then print to user "You did not enter a number. Try again!"
+When the input number matches the randomly generated number, then print to user "Correct! You win!"
+and print out the amount of guesses the user took the get the correct number.
+"""
